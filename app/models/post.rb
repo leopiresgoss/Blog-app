2e6575bc4bd85361c141c5093e4334603fcefbde
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-06 13:43:34 -0600

    Add User, Post, Comment and Like models

---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :likes
+  has_many :comments
+
+  after_save :increment_user_posts_counter
+
+  def five_recent_comments
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  private
+
+  def increment_user_posts_counter
+    author.increment!(:posts_counter)
+  end
+end
