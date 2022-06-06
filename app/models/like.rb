b15514259cd0f8df0bc895a66c1de7ec7bf1bba1
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-06 14:28:18 -0600

    Add logic to allow deletion and decrement counters

--- app/models/like.rb
+++ app/models/like.rb
@@ -2,11 +2,16 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  before_destroy :decrement_post_likes_counter
   after_save :increment_post_likes_counter
 
   private
 
   def increment_post_likes_counter
     post.increment!(:likes_counter)
+  end
+
+  def decrement_post_likes_counter
+    post.decrement!(:likes_counter)
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
-  has_many :likes
-  has_many :comments
+  has_many :likes, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
+  before_destroy :decrement_user_posts_counter
   after_save :increment_user_posts_counter
 
   def five_recent_comments
@@ -13,5 +14,9 @@ class Post < ApplicationRecord
 
   def increment_user_posts_counter
     author.increment!(:posts_counter)
+  end
+
+  def decrement_user_posts_counter
+    author.decrement!(:posts_counter)
   end
 end
