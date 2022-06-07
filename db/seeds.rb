d97b86d3ace6c1f6aa6b8cccd117824391fc738f
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-07 13:17:26 -0600

    Update views for users and posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
 # Create two users
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.')
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.')
+first_user = User.create(name: 'Tom',
+                         photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                         bio: 'Teacher from Mexico.')
+second_user = User.create(name: 'Lilly',
+                          photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                          bio: 'Teacher from Poland.')
 
 # Create four posts
 first_post = Post.create(author: first_user, title: 'Hello 1 (Tom)', text: 'This is my first post')
