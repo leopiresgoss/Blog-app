793eed045578ea9e80f3a09fbd6be3e0ed6d0cdb
Leonardo P Goss <goss.leonardo3@gmail.com>
2022-06-15 07:08:07 -0600

    Solve bug related to devise flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  root 'users#index'
-
+  devise_for :users
   resources :users, only: %i[index show] do
     resources :posts, only: %i[index new create show] do
       resources :comments, only: %i[create]
       resources :likes, only: %i[create]
     end
   end
+
+  root 'users#index'
 end
